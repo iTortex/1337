aaa9324742592cc2c703663b5b3f9fae2a42cda1
iTortex <itortex2@gmail.com>
2020-05-23 01:28:11 +0300

    Open Beta Fix

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amarcele <amarcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 17:10:43 by amarcele          #+#    #+#             */
-/*   Updated: 2020/05/17 23:18:12 by amarcele         ###   ########.fr       */
+/*   Updated: 2020/05/23 01:24:24 by amarcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (len > ft_strlen(s) - start)
-		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
 	i = 0;
 	str = malloc(len + 1);
 	if (str == NULL)
