e0a867bbdde0dce0c6822904ff6966b3ff17e777
iTortex <itortex2@gmail.com>
2020-05-25 01:01:25 +0300

    fix

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: amarcele <amarcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 16:04:44 by amarcele          #+#    #+#             */
-/*   Updated: 2020/05/24 22:59:55 by amarcele         ###   ########.fr       */
+/*   Updated: 2020/05/25 01:00:50 by amarcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ char	*ft_strnstr(const char *d, const char *l, size_t len)
 
 	i = 0;
 	j = 0;
-	if (!d[i])
-		return (NULL);
 	if (*l == '\0')
 		return ((char *)d);
-	while (d[i] != l[0] && len--)
+	while (d[i] != l[0] && len-- && d[i] != '\0')
 		i++;
 	if (d[i] == l[0] && len != 0)
 	{
-		while (d[i++] == l[j++] && len-- && l[j] != '\0')
+		while (d[i++] == l[j++] && len-- && l[j] != '\0' && d[i] != '\0')
 		{
-			if (d[i] != l[j])
+			while(d[i] != l[j])
+			{
+				i++;
 				j = 0;
+			}
 		}
 		if (l[j] == '\0')
 			return (&((char *)d)[i - ft_strlen(l)]);
